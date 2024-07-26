de28f281bcf08149f24eb6a79b818445658c1cc8
agusvlopez <agusvlopez@gmail.com>
2024-07-25 23:59:39 -0300

    add: useRef

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ const Home = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm">
         <div>
           <h1 className="text-3xl font-bold">New App</h1>
           <button onClick={addNewFox}>Add new fox</button>
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,12 +1,15 @@
+import { useRef } from "react";
+
 type Props = { image: string, alt: string };
 
 export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null)
     // const random = () => Math.floor(Math.random() * 123) + 1;
     // const image: string = `https://randomfox.ca/images/${random()}.jpg`;
 
     return (
         <>
-            <img width={320} height="auto" alt={alt} src={image} className="" />
+            <img ref={node} width={320} height="auto" alt={alt} src={image} className="" />
         </>
     )
 }
